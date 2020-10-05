e914a4a0f9bdf269804e00b15fa2adb7f88a502b
sarkis <pedropverani@hotmail.com>
2020-10-05 10:27:50 -0300

    hashing password

--- model/user.js
+++ model/user.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
-    userName: {type: String, required: true},
+    userName: {type: String, required: true, unique: true},
     userPassword: {type: String, required: true},
     createdAt: {type: Date, default: Date.now},
 });
+
+UserSchema.pre('save', async function (next) {
+    let user = this;
+    if(!user.isModified('password')) return next();
+    user.userPassword = await bcrypt.hash(user.userPassword, 10);
+    user.userName = user.email.toLowerCase();
+    return next();
+  })
+
 
 module.exports = mongoose.model('User', UserSchema);
