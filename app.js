db738e8dfb657ce35f593ef8d0127aa316d75911
ahgora <pedro.verani@ahgora.com.br>
2020-12-02 14:16:36 -0300

    trying to fix the fucking cors

--- app.js
+++ app.js
@@ -12,15 +12,15 @@ const groupRoute = require('./routes/group');
 const reportRoute = require('./routes/report');
 const database = require('./database/connection');
 const cookieParser = require('cookie-parser');
-app.use(cors());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-//     res.header('Access-Control-Allow-Headers', '*');
-//     app.use(cors());
-//     next();
-// });
-app.options('/news/scrap', cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
+    app.use(cors());
+    next();
+});
+
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(cookieParser());
