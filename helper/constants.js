6720d0291bca07b9fddf6529d21ac7812000cbe2
ahgora <pedro.verani@ahgora.com.br>
2020-12-14 19:00:33 -0300

    fixing route

--- helper/constants.js
+++ helper/constants.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const EMAIL_SERVER_URL  = `https://o4xjvb0dzd.execute-api.us-east-1.amazonaws.com/dev/mail/`;
-const NEWS_CATEGORIZER_URL_PT = 'http://0.0.0.0/pt/predict/';
-const NEWS_CATEGORIZER_URL_EN = 'http://0.0.0.0/en/predict/';
+const NEWS_CATEGORIZER_URL_PT = 'http://3.238.247.57/pt/predict/';
+const NEWS_CATEGORIZER_URL_EN = 'http://3.238.247.57/en/predict/';
 
 const randomNumberOneOrTwo = () => {
     return Math.round(Math.random() * (1 - 0) + 0);
