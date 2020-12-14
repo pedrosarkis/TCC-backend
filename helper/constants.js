93db04ef1ce295d25e034de71a3e0d01cad3ac74
ahgora <pedro.verani@ahgora.com.br>
2020-12-14 20:28:13 -0300

    fixing aws

--- helper/constants.js
+++ helper/constants.js
@@ -5,9 +5,6 @@ const NEWS_CATEGORIZER_URL_PT = 'http://3.238.247.57/pt/predict/';
 const NEWS_CATEGORIZER_URL_EN = 'http://3.238.247.57/en/predict/';
 const AWS_TOKEN = 'D8A928B2043DB77E340B523547BF16CB4AA483F0645FE0A290ED1F20AAB76257';
 
-const randomNumberOneOrTwo = () => {
-    return Math.round(Math.random() * (1 - 0) + 0);
-  }
 
 module.exports = {
     EMAIL_SERVER_URL,
